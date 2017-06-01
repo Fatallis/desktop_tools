f958b86166e243c8e8e167844140a22b49c6e92b
Fatallis <Fatallis@users.noreply.github.com>
2017-05-31 20:46:42 -0500

    Create single_upload.sh
    Created to upload a single file to google drive

---
+++ single_upload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Upload a single file to google drive
+
+# Set IFS so that it won't consider spaces as entry separators.  Without this, spaces in file/folder names can make the loop go wacky.
+IFS=$'\n'
+
+# See if the Nautilus environment variable is empty
+if [ -z $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS ]; then
+    # If it's blank, set it equal to $1
+    NAUTILUS_SCRIPT_SELECTED_FILE_PATHS=$1
+fi
+
+gnome-terminal -x /home/fatallis/.linuxbrew/bin/gdrive upload "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"
+
+
+if [ $? -eq 0 ]; then
+    notify-send -t 5000 -i /usr/share/icons/gnome/32x32/status/info.png "Uploading of $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS Finished"
+    #rm "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"
+else
+    notify-send -t 5000 -i /usr/share/icons/gnome/32x32/status/info.png "Uploading of $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS Failed"
+fi
